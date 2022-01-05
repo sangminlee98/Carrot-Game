d4527f218f4bacb76cb69fd405da083081aaf151
sangminlee98 <tkdals0975@naver.com>
2022-01-05 16:39:22 +0900

    당근 게임 구현(리팩토링 전)

--- carrot.js
+++ carrot.js
@@ -3,12 +3,17 @@ const timer = document.querySelector('.timer');
 const alertBox = document.querySelectorAll('.alert');
 const lost = document.querySelector('.lost');
 const replay = document.querySelector('.replay');
+const won = document.querySelector('.won');
 const restart = document.querySelectorAll('.restart');
+const carrots = document.querySelectorAll('.carrot');
+const bugs = document.querySelectorAll('.bug');
+const count = document.querySelector('.count');
 
 let state = false;
 
 let time = 9;
-let counting
+let counting;
+let countCarrot = 10;
 
 const timerStart = () => {
     state = !state;
@@ -18,19 +23,31 @@ const timerStart = () => {
         startTimer(); 
     } else {
         stopTimer();
+        replay.style.display = 'flex';
     }
 }
 
 const startTimer = () => {
     lost.style.display = 'none';
     startBtn.style.display = 'block';
+    carrots.forEach(carrot => {
+        carrot.style.display = 'inline';
+        randomPosition(carrot);
+    });
+
+    bugs.forEach(bug => {
+        bug.style.display = 'inline';
+        randomPosition(bug);
+    })
+    countCarrot = 10;
+    count.textContent = countCarrot;
     state = true;
     counting = setInterval(() => {
         document.querySelector('.timer').innerHTML = `00:0${time}`;
         time--;
         if(time<0){
             clearInterval(counting);
-            document.querySelector('.timer').innerHTML = '시간초과';
+            document.querySelector('.timer').innerHTML = 'Time Over';
             statusToggle();
             time = 9;
             startBtn.style.display = 'none';
@@ -42,7 +59,6 @@ const stopTimer = () => {
     startBtn.style.display = 'none';
     clearInterval(counting);
     time = 9;
-    replay.style.display = 'flex';
 }
 
 const statusToggle = () => { 
@@ -53,9 +69,24 @@ const statusToggle = () => {
     }
 }
 
+const rand = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomPosition = (object) => {
+    positionX = rand(130,1250);
+    positionY = rand(350,520);
+    object.style.top = `${positionY}px`;
+    object.style.left = `${positionX}px`;
+}
+
+
+
+
 
 
 startBtn.addEventListener('click', timerStart);
+
 restart.forEach(item => {
     item.addEventListener('click', () => {
         state = false;
@@ -64,4 +95,26 @@ restart.forEach(item => {
             box.style.display = 'none';
         })
     });
+})
+
+carrots.forEach(carrot => {
+    carrot.addEventListener('click', () => {
+        carrot.style.display = 'none';
+        countCarrot--;
+        count.textContent = countCarrot;
+        if(countCarrot === 0) {
+            state = !state;
+            stopTimer();
+            won.style.display = 'flex';
+            
+        }
+    })
+})
+
+bugs.forEach(bug => {
+    bug.addEventListener('click', () => {
+        state = !state;
+        stopTimer();
+        lost.style.display = 'flex';
+    })
 })
