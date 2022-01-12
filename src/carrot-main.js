4a6f9213647a87d23a9fc2393f30a721ac47be4f
sangminlee98 <tkdals0975@naver.com>
2022-01-12 15:51:40 +0900

    refactoring - Game class

--- src/carrot-main.js
+++ src/carrot-main.js
@@ -1,96 +1,24 @@
 import Alert from './alert.js';
-import Field from './field.js';
-import * as sound from './sound.js';
+import Game from './game.js';
 
-const startBtn = document.querySelector('.start');
-const timer = document.querySelector('.timer');
-const carrots = document.querySelectorAll('.carrot');
-const bugs = document.querySelectorAll('.bug');
-
-let state = false;
-let time = 9;
-let counting;
-let countCarrot = 10;
 const gameFinishBanner = new Alert();
+const game = new Game();
 gameFinishBanner.setClickListener(() => {
-    state = false;
-    gameStart();
+    game.start();
 })
-const field = new Field(countCarrot);
 
-const gameStart = () => {
-    state = !state;
-    if(state) {
-        timer.innerHTML = '00:10';
-        startTimer(); 
-    } else {
-        stopTimer();
-        sound.playAlert();
-        sound.pauseBgm();
-        gameFinishBanner.replayShow();
-    }
-    playImgToggle();
-}
-
-const startTimer = () => {
-    sound.playBgm();
-    startBtn.style.visibility = 'visible';
-    carrots.forEach(carrot => {
-        carrot.style.display = 'inline';
-        field.randomPosition(carrot);
-    });
-    bugs.forEach(bug => {
-        bug.style.display = 'inline';
-        field.randomPosition(bug);
-    })
-    field.initCount();
-    field.startCountText();
-    state = true;
-    counting = setInterval(() => {
-        timer.innerHTML = `00:0${time}`;
-        time--;
-        if(time<0){
-            state = !state;
-            stopTimer();
-            sound.playAlert();
-            sound.pauseBgm();
-            timer.innerHTML = 'Time Over';
-            playImgToggle();
+game.setGameStopListener(reason => {
+    switch(reason) {
+        case 'pause':
+            gameFinishBanner.replayShow();
+            break;
+        case 'lost':
             gameFinishBanner.lostShow();
-        };
-    },1000); 
-};
-const stopTimer = () => {
-    startBtn.style.visibility = 'hidden';
-    clearInterval(counting);
-    time = 9;
-}
-
-const playImgToggle = () => { 
-    if(state) {
-        startBtn.innerHTML = `<i class="fas fa-stop fa-2x"></i>`;
-    } else {
-        startBtn.innerHTML = `<i class="fas fa-play fa-2x"></i>`;
+            break;
+        case 'won':
+            gameFinishBanner.wonShow();
+            break;
+        default:
+            throw new Error('not valid reason');
     }
-}
-
-const onItemClick = (item) => {
-    if(!state) {
-        return;
-    }
-    if(item === 'carrot') {
-        state = !state;
-        stopTimer();
-        sound.pauseBgm();
-        sound.playWin();
-        gameFinishBanner.wonShow();     
-    } 
-    if(item === 'bug') {
-        state = !state;
-        sound.pauseBgm();
-        stopTimer();
-        gameFinishBanner.lostShow();   
-    }
-}
-field.setClickListener(onItemClick);
-startBtn.addEventListener('click', gameStart);
+})
---
+++ src/game.js
@@ -0,0 +1,93 @@
+import Field from './field.js';
+import * as sound from './sound.js';
+
+export default class Game {
+    constructor() {
+        this.startBtn = document.querySelector('.start');
+        this.startBtn.addEventListener('click', () => this.start());
+        this.timer = document.querySelector('.timer');
+        this.carrots = document.querySelectorAll('.carrot');
+        this.bugs = document.querySelectorAll('.bug');
+        this.state = false;
+        this.time = 9;
+        this.counting;
+        this.countCarrot = 10;
+        this.field = new Field(this.countCarrot);
+        this.field.setClickListener(this.onItemClick);
+    }
+    setGameStopListener(onGameStop) {
+        this.onGameStop = onGameStop;
+    }
+    start() {
+        this.state = !this.state;
+        if(this.state) {
+            this.timer.innerHTML = '00:10';
+            this.startTimer();
+        } else {
+            this.stopTimer();
+            sound.playAlert();
+            sound.pauseBgm();
+            this.state = false;
+            this.onGameStop && this.onGameStop('pause');
+        }
+        this.playImgToggle();
+    }
+    startTimer() {
+        sound.playBgm();
+        this.startBtn.style.visibility = 'visible';
+        this.carrots.forEach(carrot => {
+            carrot.style.display = 'inline';
+            this.field.randomPosition(carrot);
+        });
+        this.bugs.forEach(bug => {
+            bug.style.display = 'inline';
+            this.field.randomPosition(bug);
+        })
+        this.field.initCount();
+        this.field.startCountText();
+        this.state = true;
+        this.counting = setInterval(() => {
+            this.timer.innerHTML = `00:0${this.time}`;
+            this.time--;
+            if(this.time<0){
+                this.state = !this.state;
+                this.stopTimer();
+                sound.playAlert();
+                sound.pauseBgm();
+                this.timer.innerHTML = 'Time Over';
+                this.playImgToggle();
+                this.onGameStop && this.onGameStop('lost');
+            };
+        },1000); 
+    };
+    stopTimer() {
+        this.startBtn.style.visibility = 'hidden';
+        clearInterval(this.counting);
+        this.time = 9;
+    };
+    playImgToggle() { 
+        if(this.state) {
+            this.startBtn.innerHTML = `<i class="fas fa-stop fa-2x"></i>`;
+        } else {
+            this.startBtn.innerHTML = `<i class="fas fa-play fa-2x"></i>`;
+        }
+    };
+    onItemClick = (item) => {
+        if(!this.state) {
+            return;
+        }
+        if(item === 'carrot') {
+            this.state = !this.state;
+            this.stopTimer();
+            sound.pauseBgm();
+            sound.playWin();
+            this.onGameStop && this.onGameStop('won');    
+        } 
+        if(item === 'bug') {
+            this.state = !this.state;
+            sound.pauseBgm();
+            this.stopTimer();
+            this.onGameStop && this.onGameStop('lost');
+        }
+    }
+}
